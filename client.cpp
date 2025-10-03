49de93197a5f76e541444f68f6e5da9dae3ee139
harshaan999 <harshaan.chugh@gmail.com>
2025-10-03 19:33:28 -0400

    Implemented client.cpp init()

--- client.cpp
+++ client.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <filesystem>
 
 using namespace std;
+using namespace std::filesystem;
+
+void init();
 
 int main() {
     cout << "Hello world" << endl;
     return 0;
+}
+
+void init() {
+    path folder = "./gitd";
+    if (!exists(folder)) { create_directory(folder); }
+    
+    path subfolder1 = "./gitd/objects", subfolder2 = "./gitd/commits";
+    if (!exists(subfolder1)) { create_directory(subfolder1); }
+    if (!exists(subfolder2)) { create_directory(subfolder2); }
 }
