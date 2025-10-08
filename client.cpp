c4750c82fbfc1e953c8520f754e9f55baa311ffa
Arnav Kaul <68097515+kaul-arnav@users.noreply.github.com>
2025-10-08 17:26:38 -0400

    update gitd directory name

--- client.cpp
+++ client.cpp
@@ -37,16 +37,16 @@ int main(int argc, char* argv[]) {
 }
 
 void init() {
-    path folder = "./gitd";
+    path folder = ".gitd";
     if (!exists(folder)) { create_directory(folder); }
     
-    path subfolder1 = "./gitd/objects", subfolder2 = "./gitd/commits";
+    path subfolder1 = ".gitd/objects", subfolder2 = ".gitd/commits";
     if (!exists(subfolder1)) { create_directory(subfolder1); }
     if (!exists(subfolder2)) { create_directory(subfolder2); }
 }
 
 void add(string name) {
-    copy(name, "gitd/objects" + name);
+    copy(name, ".gitd/objects/" + name);
 }
 
 void commit() {
