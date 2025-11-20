e349ccd4d54a81a27e844639b928874976a9c422
Skai Nzeuton <san82@cornell.edu>
2025-11-20 18:50:05 -0500

    Create a new thread for each connection on the server; update client to incorporate these changes

--- clientserver/server.cpp
+++ clientserver/server.cpp
@@ -1,3 +1,4 @@
+#include "thread"
 #include "git_app.hpp"
 #include "tcp.hpp"
 #include "commands.hpp"
@@ -6,37 +7,50 @@
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
-int main() {
-    GitApp gitApp;
-    TcpServer server(PORT, TcpMode::SERVER);
-   
+TcpServer server(PORT, TcpMode::SERVER);
+GitApp gitApp;
 
+void handle_connection(int connected_fd) {
     while (true) {
-        std::cout << "Server listening on port " << PORT << "\n";
-
         std::vector<char> buffer = std::vector<char>(BUFFER_SIZE);
-
-        server.connect();
         
-        bool received = server.receive_message(buffer.data());
+        bool received = server.receive_message(buffer.data(), connected_fd);
         std::cout << received << std::endl;
         if (received) {
             ClientRequest request = ClientRequest();
             std::cout << "Received client message" << std::endl;
             ClientRequest::deserialize(buffer.data(), request);
             std::cout << "Deserialized message" << std::endl;
-            Command resp = gitApp.handle_client_req(Command{request.command_type, request.file_name, request.file_data.data()});
+
+            Command resp;
+            // Critical section
+            {
+                std::unique_lock<std::mutex> gitapp_lock(gitApp.gitAppMutex);
+                resp = gitApp.handle_client_req(Command{request.command_type, request.file_name, request.file_data.data()});
+            }
+
             std::cout << "Handled request" << std::endl;
             ClientReply reply = ClientReply(resp);
             // std::cout << reply.command.file_name << std::endl;
             // std::cout << reply.command.data << std::endl;
-            server.send_message(reply);
+            server.send_message(reply, connected_fd);
             std::cout << "Sent response to client" << std::endl;
         } else {
             std::cerr << "[ERROR] recv failed: " << strerror(errno) << std::endl;
-            return 1;
         }
     }
+}
 
+int main() {
+    std::cout << "Server listening on port " << PORT << "\n";
+
+    while (true) {
+        int connected_fd = server.connect(); // inside connect(), create new thread for each connection
+
+        // for each connection, run the listening/responding loop
+        std::thread t(handle_connection, connected_fd);
+        t.detach();
+    }
+    
     return 0;
 }
--- include/tcp.hpp
+++ include/tcp.hpp
@@ -26,8 +26,8 @@ public:
   TcpServer(int port, TcpMode mode);
   ~TcpServer();
 
-  void connect(const std::string &server_address = "");
+  int connect(const std::string &server_address = "");
 
-  void send_message(Message& message, const std::string &dest_address = "");
-  bool receive_message(char *buffer, const std::string &source_address = "");
+  void send_message(Message& message, const int connected_fd);
+  bool receive_message(char *buffer, const int connected_fd);
 };
